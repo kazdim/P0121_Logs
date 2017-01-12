b90f9332c4beeee920ac543007269d342e1b404f
kazdim <dmytri2009@gmail.com>
2017-01-12 16:10:56 +0200

    Работа LogCat

--- app/src/main/java/com/mydomain/dk/p0121_logs/MainActivity.java
+++ app/src/main/java/com/mydomain/dk/p0121_logs/MainActivity.java
@@ -2,28 +2,33 @@ package com.mydomain.dk.p0121_logs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import static android.R.attr.id;
-
 public class MainActivity extends AppCompatActivity {
 
     TextView myTextView;
-    Button Btn1;
-    Button Btn2;
-    Button Btn3;
+    Button Button1;
+    Button Button2;
+    Button Button3;
+
+    private static final String TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d(TAG, "Найдем View-переменные");
+
         myTextView = (TextView) findViewById(R.id.myTextView);
-        Btn1 = (Button) findViewById(R.id.btn1);
-        Btn2 = (Button) findViewById(R.id.btn2);
-        Btn3 = (Button) findViewById(R.id.btn3);
+        Button1 = (Button) findViewById(R.id.btn1);
+        Button2 = (Button) findViewById(R.id.btn2);
+        Button3 = (Button) findViewById(R.id.btn3);
+
+
 
 
         View.OnClickListener myOnClickListener = new View.OnClickListener() {
@@ -31,18 +36,31 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 switch (view.getId()) {
                 case R.id.btn1: myTextView.setText(R.string.text1);
+                    Log.d(TAG, "Обработаем нажатие кнопки 1");
                     break;
                 case R.id.btn2: myTextView.setText(R.string.text2);
+                    Log.d(TAG, "Обработаем нажатие кнопки 2");
                     break;
-                case R.id.btn3: myTextView.setText(R.string.text3);
-                break;
                 }
             }
         };
 
-        Btn1.setOnClickListener(myOnClickListener);
-        Btn2.setOnClickListener(myOnClickListener);
-        Btn3.setOnClickListener(myOnClickListener);
+        Button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    int i = 6/0;
+                    myTextView.setText("Результат деления - " + i);
+                } catch (Exception e) {
+                    Log.d(TAG, "Делить на ноль нельзя!", e);
+                }
+
+            }
+        });
+
+        Button1.setOnClickListener(myOnClickListener);
+        Button2.setOnClickListener(myOnClickListener);
+
     }
 
 }
